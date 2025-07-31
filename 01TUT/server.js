cf827e51a4426fbd3b7f62d204317accd9806d88
etsuba19 <etsuba24@gmail.com>
2025-07-31 11:13:45 +0300

    Tutorial 01

---
+++ 01TUT/server.js
@@ -0,0 +1,10 @@
+
+console.log("Hello Welcome to my world, let's get started")
+
+const { add, subtract, multiply, divide} = require('./math')
+
+console.log("Here is what we are getting with math module that we created.")
+console.log(add(23, 30))
+console.log(subtract(23, 30))
+console.log(multiply(23, 30))
+console.log(divide(23, 30))
